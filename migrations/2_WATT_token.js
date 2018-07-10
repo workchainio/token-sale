e72174c835022d1fff056cb15191b55d12a6d5be
ipetrovic11 <ivan@workchain.io>
2018-07-10 10:42:56 +0200

    Pointing to real WATT address on live

--- migrations/2_WATT_token.js
+++ migrations/2_WATT_token.js
@@ -5,8 +5,7 @@ module.exports = async function(deployer, network, accounts) {
   if (network == "staging") {
     await TokenContract.at('0x0290fb167208af455bb137780163b7b7a9a10c16');
   } else if(network == "live") {
-    throw "Trying to deploy unimplemented constructor to the live network";
-    await TokenContract.at('0x0');
+    await TokenContract.at('0x829a4ca1303383f1082b6b1fb937116e4b3b5605');
   } else {
     await deployer.deploy(TokenContract);
   }
